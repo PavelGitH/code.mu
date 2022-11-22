40ffc019c435c102cc516e671104ee3eacf4e5ec
PavelGitH <linevich_pavel@mail.ru>
2022-11-22 22:09:36 +0300

    folder change

---
+++ JavaScript/BasicJavaScriptTutorial/155/script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+console.log('1-----------------------------------');
+let num1 = 12345;
+let arr1 = String(num1).split('');
+let sum1 = 0;
+for (let digit1 of arr1) {
+    sum1 += Number(digit1);
+}
+console.log(sum1);
+
+console.log('2-----------------------------------');
+let num2 = 12345;
+let arr2 = String(num2).split('');
+let sum2 = 0;
+for (let digit2 of arr2) {
+    sum2 += Number(digit2);
+}
+console.log(sum2);
+
+console.log('3-----------------------------------');
+let num3 = 12345;
+let arr3 = String(num3).split('');
+let sum3 = 0;
+for (let digit3 of arr3) {
+    sum3 += Number(digit3);
+}
+console.log(sum3);
+
+console.log('4-----------------------------------');
+let num4 = 12345;
+let arr4 = String(num4).split('');
+let sum4 = 0;
+for (let digit4 of arr4) {
+    sum4 += Number(digit4);
+}
+console.log(sum4);
+
+console.log('5-----------------------------------');
+let num5 = 12345;
+let arr5 = String(num5).split('');
+let prod5 = 1;
+for (let digit5 of arr5) {
+    prod5 *= Number(digit5);
+}
+console.log(prod5);
